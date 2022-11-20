a939872d406b6e8131fb70f223c12bb09dcd93d5
Dzung <duydung25082003@gmail.com>
2022-11-19 18:57:36 -0800

    add database

--- app/src/main/java/com/example/project_ewallet/DBHelper.java
+++ app/src/main/java/com/example/project_ewallet/DBHelper.java
@@ -1,2 +1,56 @@
-package com.example.project_ewallet;public class DBHelper {
+package com.example.project_ewallet;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String INCOME_TABLE = "INCOME";
+    public static final String EXPENSE_TABLE = "EXPENSE";
+
+    // Table columns
+    public static final String ID = "_id";
+    public static final String DATE = "fn";
+    public static final String AMOUNT = "ln";
+    public static final String CATEGORIES = "salary";
+
+
+    // Database Information
+    static final String DB_NAME = "EWALLET.DB";
+
+    // database version
+    static final int DB_VERSION = 1;
+
+    // Creating table query
+    private static final String CREATE_INCOME = "create table " + INCOME_TABLE + "(" + ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + DATE + " DATE NOT NULL, "
+            + AMOUNT + " DOUBLE(9,2) NOT NULL, "
+            + CATEGORIES + " TEXT NOT NULL );";
+
+    private static final String CREATE_EXPENSE = "create table " + EXPENSE_TABLE + "(" + ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + DATE + " DATE NOT NULL, "
+            + AMOUNT + " DOUBLE(9,2) NOT NULL, "
+            + CATEGORIES + " TEXT NOT NULL );";
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDB) {
+        Log.d("DBHelper","onCreate");
+
+        sqLiteDB.execSQL(CREATE_INCOME);
+        sqLiteDB.execSQL(CREATE_EXPENSE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDB, int oldVersion, int newVerion) {
+        Log.d("version change", "oldVersion: " + oldVersion + ", newVersion: " + newVerion);
+        sqLiteDB.execSQL("DROP TABLE IF EXISTS " + INCOME_TABLE);
+        sqLiteDB.execSQL("DROP TABLE IF EXISTS " + EXPENSE_TABLE);
+        onCreate(sqLiteDB);
+    }
 }
--- app/src/main/java/com/example/project_ewallet/ui/future/FutureFragment.java
+++ app/src/main/java/com/example/project_ewallet/ui/future/FutureFragment.java
@@ -5,15 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.project_ewallet.R;
 import com.example.project_ewallet.databinding.FragmentBudgetBinding;
+import com.github.mikephil.charting.charts.PieChart;
 
 
 public class FutureFragment extends Fragment {
 
+    PieChart pieChart;
         private FragmentBudgetBinding binding;
+
 
         public View onCreateView(@NonNull LayoutInflater inflater,
                                  ViewGroup container, Bundle savedInstanceState) {
@@ -21,6 +27,7 @@ public class FutureFragment extends Fragment {
 
             binding = FragmentBudgetBinding.inflate(inflater, container, false);
             View root = binding.getRoot();
+            pieChart = (PieChart) getView().findViewById(R.id.chart);
 
 
             return root;
