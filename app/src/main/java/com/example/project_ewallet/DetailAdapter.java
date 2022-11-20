da661f605167914b4c3c161e29fcc05e55d31dce
Emilia Hoang <97384201+emilia84@users.noreply.github.com>
2022-11-19 20:50:58 -0800

    4th layout

--- app/src/main/java/com/example/project_ewallet/DetailAdapter.java
+++ app/src/main/java/com/example/project_ewallet/DetailAdapter.java
@@ -1,31 +1,51 @@
 package com.example.project_ewallet;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.ViewHolder>{
+    private ArrayList<String[]> list;
+    public DetailAdapter(ArrayList<String[]> data){
+        super();
+        this.list = data;
+    }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.item_layout, parent, false);
+        return new ViewHolder(listItem);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        final String[] item = list.get(position);
+        holder.txtCat.setText(item[0]);
+        holder.txtSal.setText(item[1]);
+        holder.txtDate.setText(item[2]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtCat;
+        public TextView txtSal;
+        public TextView txtDate;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            this.txtCat = (TextView) itemView.findViewById(R.id.txtCat);
+            this.txtSal = (TextView) itemView.findViewById(R.id.txtSal);
+            this.txtDate = (TextView) itemView.findViewById(R.id.txtDate);
         }
     }
 }
--- app/src/main/java/com/example/project_ewallet/ui/setUpRecur/SetUpRecurFragment.java
+++ app/src/main/java/com/example/project_ewallet/ui/setUpRecur/SetUpRecurFragment.java
@@ -1,28 +1,58 @@
 package com.example.project_ewallet.ui.setUpRecur;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.example.project_ewallet.databinding.FragmentBudgetBinding;
 
-public class SetUpRecurFragment extends Fragment {
-    private FragmentBudgetBinding binding;
+import com.example.project_ewallet.R;
+import com.example.project_ewallet.databinding.FragmentBudgetBinding;
+import com.example.project_ewallet.databinding.FragmentSetuprecurBinding;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class SetUpRecurFragment extends Fragment{
+    private FragmentSetuprecurBinding binding;
+    private TextView reservation;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
 
-        binding = FragmentBudgetBinding.inflate(inflater, container, false);
+        binding = FragmentSetuprecurBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
+        reservation = (TextView) root.findViewById(R.id.txtRes);
+        Button but = (Button) root.findViewById(R.id.btnDate);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(view.getContext(),d,c.get(Calendar.YEAR),
+                        c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)).show();
+                reservation.setText(fmtDate.format(c.getTime()));
+            }
+        });
 
         return root;
     }
+    Calendar c = Calendar.getInstance();
+    DateFormat fmtDate = DateFormat.getDateInstance();
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            c.set(Calendar.YEAR, year);
+            c.set(Calendar.MONTH, monthOfYear);
+            c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        }
+    };
 
     @Override
     public void onDestroyView() {
