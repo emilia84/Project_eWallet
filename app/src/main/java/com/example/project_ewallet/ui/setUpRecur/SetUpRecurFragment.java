613c946cbae2c7deb13b066e8065d7a524b29632
Emilia Hoang <97384201+emilia84@users.noreply.github.com>
2022-12-03 21:18:16 -0800

    #19

--- app/src/main/java/com/example/project_ewallet/ui/setUpRecur/SetUpRecurFragment.java
+++ app/src/main/java/com/example/project_ewallet/ui/setUpRecur/SetUpRecurFragment.java
@@ -1,6 +1,7 @@
 package com.example.project_ewallet.ui.setUpRecur;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,14 +18,13 @@ import androidx.fragment.app.Fragment;
 
 import com.example.project_ewallet.DBManager;
 import com.example.project_ewallet.R;
-import com.example.project_ewallet.databinding.FragmentBudgetBinding;
 import com.example.project_ewallet.databinding.FragmentSetuprecurBinding;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class SetUpRecurFragment extends Fragment{
+
     private FragmentSetuprecurBinding binding;
     private TextView reservation;
     private DBManager dbManager;
@@ -54,16 +54,6 @@ public class SetUpRecurFragment extends Fragment{
 
         rMonth = (RadioButton) root.findViewById(R.id.radMonth);
         rWeek = (RadioButton) root.findViewById(R.id.radWeek);
-        miscel = (Button) root.findViewById(R.id.btnMiscel);
-        bill = (Button) root.findViewById(R.id.btnBill);
-        clothes = (Button) root.findViewById(R.id.btnClot);
-        food = (Button) root.findViewById(R.id.btnFood);
-        health = (Button) root.findViewById(R.id.btnHealth);
-        house = (Button) root.findViewById(R.id.btnHouse);
-        transport = (Button) root.findViewById(R.id.btnTrans);
-        toilet = (Button) root.findViewById(R.id.btnToilet);
-        entertain = (Button) root.findViewById(R.id.btnEnt);
-        amount = (EditText) root.findViewById(R.id.txtUpdateAmount);
 
         dbManager = new DBManager(getContext());
         dbManager.open();
@@ -84,6 +74,24 @@ public class SetUpRecurFragment extends Fragment{
     };
 
     @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // save views as variables in this method
+        // "view" is the one returned from onCreateView
+        amount = (EditText) view.findViewById(R.id.txtAmountRecur);
+        miscel = (Button) view.findViewById(R.id.btnMiscel);
+        bill = (Button) view.findViewById(R.id.btnBill);
+        clothes = (Button) view.findViewById(R.id.btnClot);
+        food = (Button) view.findViewById(R.id.btnFood);
+        health = (Button) view.findViewById(R.id.btnHealth);
+        house = (Button) view.findViewById(R.id.btnHouse);
+        transport = (Button) view.findViewById(R.id.btnTrans);
+        toilet = (Button) view.findViewById(R.id.btnToilet);
+        entertain = (Button) view.findViewById(R.id.btnEnt);
+        amount = (EditText) view.findViewById(R.id.txtAmountRecur);
+
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         dbManager.close();
@@ -93,46 +101,50 @@ public class SetUpRecurFragment extends Fragment{
     //Code Interval!!!!
     public void chooseInterval(View view){
         if(rMonth.isChecked()){
-//            addRecurExpense(view);
+
         }
         else if(rWeek.isChecked()){
-            //
+
         }
     }
+
     public void addRecurExpense(View v) {
-//        String catVal = "";
-//        switch (v.getId()) {
-//            case R.id.btnMiscel:
-//                catVal = miscel.getText().toString();
-//                break;
-//            case R.id.btnBill:
-//                catVal = bill.getText().toString();
-//                break;
-//            case R.id.btnClot:
-//                catVal = clothes.getText().toString();
-//                break;
-//            case R.id.btnFood:
-//                catVal = food.getText().toString();
-//                break;
-//            case R.id.btnHealth:
-//                catVal = health.getText().toString();
-//                break;
-//            case R.id.btnHouse:
-//                catVal = house.getText().toString();
-//                break;
-//            case R.id.btnTrans:
-//                catVal = transport.getText().toString();
-//                break;
-//            case R.id.btnToilet:
-//                catVal = toilet.getText().toString();
-//                break;
-//            case R.id.btnEnt:
-//                catVal = entertain.getText().toString();
-//                break;
-//            default:
+        String catVal = "";
+        if(v != null){
+            switch (v.getId()) {
+                case R.id.btnMiscel:
+                    catVal = miscel.getText().toString();
+                    break;
+                case R.id.btnBill:
+                    catVal = bill.getText().toString();
+                    break;
+                case R.id.btnClot:
+                    catVal = clothes.getText().toString();
+                    break;
+                case R.id.btnFood:
+                    catVal = food.getText().toString();
+                    break;
+                case R.id.btnHealth:
+                    catVal = health.getText().toString();
+                    break;
+                case R.id.btnHouse:
+                    catVal = house.getText().toString();
+                    break;
+                case R.id.btnTrans:
+                    catVal = transport.getText().toString();
+                    break;
+                case R.id.btnToilet:
+                    catVal = toilet.getText().toString();
+                    break;
+                case R.id.btnEnt:
+                    catVal = entertain.getText().toString();
+                    break;
+                default:
 //                throw new RuntimeException();
-//        }
-//        double amountVal = Double.parseDouble(amount.getText().toString());
-//        dbManager.insertExpense(catVal, amountVal);
+            }
+        }
+
+        double amountVal = Double.parseDouble(amount.getText().toString());
+        dbManager.insertExpense(catVal, amountVal);
     }
 }
